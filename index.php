9db19cdd61ff44fe81eb8db6ebfaf023b237c36a
RahimiillongT <rahimiillong@student.ub.ac.id>
2024-04-17 19:45:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Perpustakaan</title>
+</head>
+<body>
+    <h1>Selamat Datang di Perpustakaan</h1>
+
+    <h2>Pilih Fitur:</h2>
+    <ul>
+        <li><a href="fitur.php?fitur=cari">Pencarian</a></li>
+        <li><a href="fitur.php?fitur=pinjam">Peminjaman</a></li>
+    </ul>
+</body>
+</html>
