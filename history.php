1038cf3a3e3ffe4f76a947a07652a978badd9c62
RahimiillongT <rahimiillong@student.ub.ac.id>
2024-04-21 10:31:56 +0700

    waduh kayanya sih selesai anjay

--- history.php
+++ history.php
@@ -1,29 +1,25 @@
 <?php
-$link = new mysqli("127.0.0.1", "root", "", "perpustakaan_terstruktur");
+$link = new mysqli("127.0.0.1", "root", "", "perpustakaan");
 
-$query = "SELECT buku.id, buku.judul, dipinjam.tanggal_peminjaman, pengembalian.tanggal_pengembalian 
-          FROM buku 
-          JOIN dipinjam ON buku.id = dipinjam.buku_id 
-          JOIN pengembalian ON dipinjam.buku_id = pengembalian.buku_id";
+$query = "SELECT buku.id, buku.judul, riwayat.tanggal_peminjaman, riwayat.tanggal_pengembalian 
+        FROM riwayat 
+        JOIN buku ON riwayat.buku_id = buku.id";
+
 
 $result = $link->query($query);
-?>
 
-<table border="1">
-<tr><td>No</td><td>ID Buku</td><td>Judul Buku</td><td>Tanggal Peminjaman</td><td>Tanggal Pengembalian</td></tr>
-<?php
+
+echo "<table border='1'>";
+echo "<tr><td>No</td><td>ID Buku</td><td>Judul Buku</td><td>Tanggal Peminjaman</td><td>Tanggal Pengembalian</td></tr>";
 $i = 1;
 while ($row = $result->fetch_assoc()) {
     echo "<tr>";
-    echo "<td>$i</td><td>{$row['id buku']}</td><td>{$row['judul']}</td><td>{$row['tanggal_peminjaman']}</td><td>{$row['tanggal_pengembalian']}</td></tr>";
+    echo "<td>$i</td><td>{$row['id']}</td><td>{$row['judul']}</td><td>{$row['tanggal_peminjaman']}</td><td>{$row['tanggal_pengembalian']}</td></tr>";
     $i++;
 }
-?>
-</table> <br>
+echo "</table> <br>";
 
-<a href='./index.php'>KEMBALI KE DASHBOARD</a>
-
-<?php
+echo "<a href='./index.php'>KEMBALI KE DASHBOARD</a>";
 $result->free_result();
 $link->close();
 ?>
--- pengembalian.php
+++ pengembalian.php
@@ -1,9 +1,17 @@
 <?php
-$link = new mysqli("127.0.0.1", "root", "", "perpustakaan_terstruktur");
+$link = new mysqli("127.0.0.1", "root", "", "perpustakaan");
 
 
 if(isset($_GET['id'])) {
     $id_buku = $_GET['id'];
+
+    $query_select_tanggal = "SELECT tanggal_peminjaman FROM dipinjam WHERE buku_id = ?";
+    $stmt_tanggal = $link->prepare($query_select_tanggal);
+    $stmt_tanggal->bind_param('i', $id_buku);
+    $stmt_tanggal->execute();
+    $result = $stmt_tanggal->get_result();
+    $row_tanggal = $result->fetch_assoc();
+    $tanggal_peminjaman = $row_tanggal['tanggal_peminjaman'];
 
     $query_delete = "DELETE FROM dipinjam WHERE buku_id = ?";
     $stmt_delete = $link->prepare($query_delete);
@@ -14,6 +22,11 @@ if(isset($_GET['id'])) {
     $stmt_insert = $link->prepare($query_insert);
     $stmt_insert->bind_param("i", $id_buku);
     $stmt_insert->execute();
+
+    $query_riwayat = "INSERT INTO riwayat (buku_id, tanggal_peminjaman, tanggal_pengembalian) VALUES (?, ?, NOW())";
+    $stmt_riwayat = $link->prepare($query_riwayat);
+    $stmt_riwayat->bind_param("is", $id_buku, $tanggal_peminjaman);
+    $stmt_riwayat->execute();
 
     echo "Buku telah berhasil dikembalikan.<br><br>";
 
